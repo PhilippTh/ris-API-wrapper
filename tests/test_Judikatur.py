11a37de78f08e2f8d072f51b04aef5cd0df6e33e
PhilippTh <philipp@thumfart.info>
2021-08-08 19:34:52 +0200

    updated tests

--- tests/test_Judikatur.py
+++ tests/test_Judikatur.py
@@ -1,15 +1,30 @@
 from risApiWrapper.Judikatur import *
 
+def testJustizEntscheidungstext():
+    """Test an API call to get a Entscheidungstext"""
 
-def testJudikaturEntscheidungstext():
-    """Test an API call to get a case"""
-
-    wrapperInstance = Justiz(caseNumber="13Os65/21i")
+    wrapperInstance = Justiz(caseNumber="5Ob234/20b", rechtssaetze=False)
     response = wrapperInstance.info()
 
     assert isinstance(response, list)
     assert isinstance(response[0], dict)
-    assert response[0]["caseNumber"][0] == "13Os65/21i"
-    assert response[0]["type"] == "Entscheidungstext"
+    for i in response: 
+        assert "5Ob234/20b" in i["caseNumber"]
+    for i in response:
+        assert i["type"] == "Entscheidungstext"
+    assert response[0].keys() >= {"type", "caseNumber", "europeanCaseLawIdentifier", "judicialBody", "decisionDate", "published", "edited", "legalNorms", "dokumentUrl", "contentUrls"}
+
+def testJustizRechtssatz():
+    """Test an API call to get a Rechtssatz"""
+
+    wrapperInstance = Justiz(caseNumber="5Ob234/20b", entscheidungstexte=False)
+    response = wrapperInstance.info()
+
+    assert isinstance(response, list)
+    assert isinstance(response[0], dict)
+    for i in response:
+        assert "5Ob234/20b" in i["caseNumber"]
+    for i in response:
+        assert i["type"] == "Rechtssatz"
     assert response[0].keys() >= {"type", "caseNumber", "europeanCaseLawIdentifier", "judicialBody", "decisionDate", "published", "edited", "legalNorms", "dokumentUrl", "contentUrls"}
 
